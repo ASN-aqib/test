ab4b4576cfacba9a02128627b5e5b0f0e286ffc3
abbasqamar <abbasqamar@msn.com>
2026-02-02 03:51:05 -0800

    second

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+
+   @Get('api/what')
+   getho(): string {
+    return "What are you doing";
+  }
+
 }
